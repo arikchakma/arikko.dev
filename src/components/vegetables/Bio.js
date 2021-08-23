15170143923872c164b477a303e6a38bb0224ba1
Arik Chakma <arikchangma@gmail.com>
2021-08-24 03:59:03 +0600

    Added Bio

---
+++ src/components/vegetables/Bio.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GatsbyImage } from 'gatsby-plugin-image/dist/src/components/gatsby-image.browser';
+
+export default function Bio() {
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Heading>Arik Chakma</Heading>
+          <Spoiler>A frontend chef who is cooking his world!</Spoiler>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.main``;
+
+const ContentWrapper = styled.div``;
+
+const TextWrapper = styled.div`
+  max-width: 350px;
+`;
+
+const Heading = styled.h1``;
+
+const Spoiler = styled.p``;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
 import BlogSection from '../components/sections/BlogSection';
+import Bio from '../components/vegetables/Bio';
 
 function IndexPage() {
   return (
     <Layout>
+      <Bio />
       <BlogSection />
     </Layout>
   );
