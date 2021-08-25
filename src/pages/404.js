6363f2d51aba34551d91e1ef89e4b010729b3ea5
Arik Chakma <arikchangma@gmail.com>
2021-08-25 20:15:49 +0600

    Added a text

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,6 +8,7 @@ function NotFoundPage() {
       <SEO title="404: Not found" />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>What the hack?</p>
     </Layout>
   );
 }
