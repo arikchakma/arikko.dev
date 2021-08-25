cd3050f0098bd5fc00da3379962370b7a9437b9f
Arik Chakma <arikchangma@gmail.com>
2021-08-25 20:03:19 +0600

    Fixed Code Block css

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import GlobalStyle from '../styles/GlobalStyle';
 import Footer from './Footer';
 import Header from './Header';
-import './layout.css';
 
 function Layout({ children }) {
   return (
