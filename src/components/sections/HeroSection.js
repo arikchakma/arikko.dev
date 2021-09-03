0a4dbe609dcc119d3eb93763826b2334e2367277
Arik Chakma <arikchangma@gmail.com>
2021-09-03 15:01:16 +0600

    Added bad ass slogan

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -9,42 +9,48 @@ export default function HeroSection() {
       <SEO title="A Frontend Chef's World" />
       <ContentWrapper>
         <Heading>
-          Chef
-          <br /> is <TextAnimation />!
+          <TextAnimation /> this dish like mom does!
         </Heading>
       </ContentWrapper>
+      <HiringWrapper>
+        <P>Wanna hire me for your delicious dish?</P>
+        <ContactMom>
+          <ContactLink href="/">contact</ContactLink> my mom!
+        </ContactMom>
+      </HiringWrapper>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  background: #07090e;
+  position: relative;
+  background: #0d1321;
 `;
 
 const ContentWrapper = styled.div`
   max-width: 1440px;
   height: 100vh;
   margin: 0 auto;
-  padding: 200px 120px;
-  display: flex;
+  padding: 0 120px;
+  display: grid;
   justify-content: center;
   align-items: center;
 
   @media screen and (max-width: 768px) {
-    padding: 200px 80px;
+    padding: 0 80px;
   }
 
   @media screen and (max-width: 450px) {
-    padding: 200px 30px;
+    padding: 0 30px;
   }
 
   @media screen and (max-width: 320px) {
-    padding: 200px 20px;
+    padding: 0 20px;
   }
 `;
 
 const Heading = styled.h1`
-  min-width: 350px;
+  max-width: 550px;
   font-family: Recoleta;
   font-style: normal;
   font-weight: bold;
@@ -63,7 +69,33 @@ const Heading = styled.h1`
     font-size: 35px;
   }
 
-  @media screen and (max-width: 320px) {
-    min-width: 300px;
+  /* @media screen and (max-width: 320px) {
+    /* min-width: 300px; */
+  } */
+`;
+
+const HiringWrapper = styled.div`
+  position: absolute;
+  bottom: 50px;
+  width: 100%;
+
+  padding: 0 20px;
+  color: #405380;
+  font-family: 'Recoleta';
+  font-size: 25px;
+  text-align: center;
+`;
+
+const P = styled.p``;
+
+const ContactMom = styled.p``;
+
+const ContactLink = styled.a`
+  background: linear-gradient(270.81deg, #ce63c3 -30.3%, #3b2ed0 95.45%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  :hover {
+    border-bottom: 1px solid #405380;
   }
 `;
