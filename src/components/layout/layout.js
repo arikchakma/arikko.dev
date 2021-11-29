3a33bb0c191a027f830c9dfac08b7cafe7cd71ac
Arik Chakma <arikchangma@gmail.com>
2021-11-30 04:15:14 +0600

    🎉 Hidden Header

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './Header';
+// import Header from './Header';
 import './layout.css';
 
 function Layout({ children }) {
   return (
     <Wrapper>
-      <Header />
+      {/* <Header /> */}
       {children}
     </Wrapper>
   );
